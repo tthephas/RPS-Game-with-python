11739daf80bb49a629b3d6f4b7cf9b20b51c26ba
Josh Thephasdin <47870092+tthephas@users.noreply.github.com>
2023-05-30 20:14:25 -0500

    game finished with error catching and ascii pics working

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -29,15 +29,20 @@ scissors = '''
 ---.__(___)
 '''
 
+game_images = [rock, paper, scissors]
+
 user_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors. \n'))
-
-computer_choice = random.randint(0, 2)
-
-print(f'Computer chose {computer_choice}')
-
 if user_choice >= 3 or user_choice < 0:
     print('You typed an invalid number. You lose!')
-elif user_choice == 0 and computer_choice == 2:
+else:
+    print(game_images[user_choice])
+
+    computer_choice = random.randint(0, 2)
+    print(f'Computer chose: ')
+    print(game_images[computer_choice])
+
+
+if user_choice == 0 and computer_choice == 2:
     print('You win!')
 elif computer_choice == 0 and user_choice == 2:
     print('You lose')

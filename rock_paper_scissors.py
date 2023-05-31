52cb65dc46f79855ba04a524f12a1d2bbf916537
Josh Thephasdin <47870092+tthephas@users.noreply.github.com>
2023-05-30 20:09:40 -0500

    if scenarios done

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -29,8 +29,21 @@ scissors = '''
 ---.__(___)
 '''
 
-user_choice = input('What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors. \n')
+user_choice = int(input('What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors. \n'))
 
 computer_choice = random.randint(0, 2)
 
 print(f'Computer chose {computer_choice}')
+
+if user_choice >= 3 or user_choice < 0:
+    print('You typed an invalid number. You lose!')
+elif user_choice == 0 and computer_choice == 2:
+    print('You win!')
+elif computer_choice == 0 and user_choice == 2:
+    print('You lose')
+elif computer_choice > user_choice:
+    print('You lose, sorry. :(')
+elif user_choice > computer_choice:
+    print('You win!')
+elif computer_choice ==  user_choice:
+    print('It is a draw.')
